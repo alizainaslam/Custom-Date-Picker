4ad59ce9713c2d698d3d6f4dce63ea4923a248a3
Ali Zain <alizain10166@gmail.com>
2024-03-30 15:56:50 +0100

    Bug fixed for month's value, if year is bigger than the current year

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,51 @@
+/**
+ * DOM element representing the year select input.
+ * @type {HTMLSelectElement}
+ */
 const setYear = document.querySelector("#year");
-const setMonth = document.querySelector("#month");
-const setWeekday = document.querySelector("#weekday");
-document.querySelector("form").addEventListener("submit", submitForm);
 
 /**
- * Create new Date
- * @type {Date} newDate
+ * DOM element representing the month select input.
+ * @type {HTMLSelectElement}
+ */
+const setMonth = document.querySelector("#month");
+
+/**
+ * DOM element representing the weekday select input.
+ * @type {HTMLSelectElement}
+ */
+const setWeekday = document.querySelector("#weekday");
+
+/**
+ * Function to handle form submission.
+ * @param {Event} event - The submit event,
+ */
+function submitForm(event) {
+  event.preventDefault();
+  console.log(setYear.value, setMonth.value);
+}
+
+/**
+ * Create new Date object
  */
 const newDate = new Date();
 
 /**
- * Section for YEAR field 1️⃣
- *
- * Set minimum year for currentYear,
- * set maximum year for currentYear + 5,
- *
- * @type {number} - currentYear
- * @type {number} - maximum
- *
- * Create array and push all year <min / max>,
- * @type {Array} - listOfYears
+ * The current year
+ * @type {number}
  */
-
 const currentYear = newDate.getFullYear();
+
+/**
+ * The maximun year allowed, which is the current year plus 5.
+ * @type {number}
+ */
 const maximumYear = currentYear + 5;
 
+/**
+ * Array to store the list of years
+ * @type {number[]}
+ */
 const listOfYears = [];
 
 let i = currentYear - 1;
@@ -33,6 +54,7 @@ while (i < maximumYear) {
   listOfYears.push(i);
 }
 
+// Populate the year select input with options.
 listOfYears.forEach((year) => {
   const option = document.createElement("option");
   option.value = year;
@@ -41,18 +63,17 @@ listOfYears.forEach((year) => {
 });
 
 /**
- * Section for MONTH field 1️2️⃣
- *
- * Set currentMonth for current month,
- * @type {number} - currentMonth
- *
- * Create array and push list of month names,
- * @type {Array} - listOfMonths
+ * The current month
  */
 const currentMonth = newDate.getMonth();
 
+/**
+ * Array to store the list of month names.
+ * @type {string[]}
+ */
 const listOfMonths = [];
 
+// Populate the list of month names.
 Array.from({ length: 12 }, (_, i) => {
   return listOfMonths.push(
     new Date(currentYear, i).toLocaleDateString(undefined, {
@@ -61,24 +82,33 @@ Array.from({ length: 12 }, (_, i) => {
   );
 });
 
+// Populate the year select input with options.
 listOfMonths.forEach((month, index) => {
   const option = document.createElement("option");
   option.value = month;
   option.textContent = month;
   setMonth.appendChild(option);
 
+  // Disable past months
   if (index < currentMonth) {
     option.disabled = true;
   }
 });
 
+// Set the default value of the month select input.
 setMonth.value = listOfMonths[currentMonth];
 
-/**
- * Handle submission form
- * @type {Event} event
- */
-function submitForm(event) {
-  event.preventDefault();
-  console.log(setYear.value, setMonth.value);
-}
+// Handle past month's values if year is bigger than the current year.
+setYear.addEventListener("change", () => {
+  const selectedYear = parseInt(setYear.value);
+  Array.from(setMonth).forEach((month, index) => {
+    if (selectedYear > currentYear && index < currentMonth) {
+      month.disabled = false;
+    } else if (selectedYear <= currentYear && index < currentMonth) {
+      month.disabled = true;
+    }
+  });
+});
+
+// Add event listener to the form for submission.
+document.querySelector("form").addEventListener("submit", submitForm);
